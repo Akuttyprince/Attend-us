202037de7236cb11fdb4209d87b5a8b095d877ee
Akuttyprince <yuvarajpro213@gmail.com>
2025-04-14 12:26:26 +0530

    checking for access

--- Backend/Models/TeacherModel.js
+++ Backend/Models/TeacherModel.js
@@ -4,7 +4,7 @@ const TeacherSchema = new mongoose.Schema({
      username:{ trype: string, required: true , unique: true},
      password:{type: string, required: true},  
 
-
+// hello
 });
 
 module.exports = mongoose.Model('TeacherModel', TeacherSchema);
